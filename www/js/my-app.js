22e2bb13695f22f270a32e4d8057e0bf026cfe1c
Balays33 <40806995+Balays33@users.noreply.github.com>
2019-03-23 20:42:54 +0000

    version
    update

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -41,3 +41,47 @@ $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
     myApp.alert('Here comes About page');
 })
+
+// geoLocation function
+function geoLocation(){
+   
+    navigator.geolocation.getCurrentPosition(geoCallback, onError)
+
+}
+
+// onSuccess Callback
+    //   This method accepts a `Position` object, which contains
+    //   the current GPS coordinates
+    //
+    function geoCallback(position) {
+        console.log(position);
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                            'Longitude: ' + position.coords.longitude     + '<br />' +
+                            '<hr />'      + element.innerHTML;
+    }
+ 
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+        console.log(error);
+    }
+ 
+    // Options: throw an error if no update is received every 30 seconds.
+    //
+   // var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+
+
+
+
+
+
+function getTime(){
+  
+    //alert('Hello');
+    var d = new Date();
+    document.getElementById("demo").innerHTML = d;
+    console.log(d);
+}
